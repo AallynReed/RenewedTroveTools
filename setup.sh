130e5d5781006b7525cc7b7fcc5c3b2785db97fd
ThePotatoPvP <corentin.sallin@gmail.com>
2024-03-06 15:48:52 +0100

    window opens omg we pogging 🔥🔥🔥🔥

---
+++ setup.sh
@@ -0,0 +1,7 @@
+echo -e "Creating a python venv..."
+python -m venv venv
+echo -e "Installing packages..."
+venv/bin/pip install -r requirements.txt > /dev/null
+
+echo -e "Done ! Do not forget to create a simlink by doing : \e[92msudo ln -s /usr/local/lib/libmpv.so /usr/local/lib/libmpv.so.1\e[0m"
+# The simlink is a temporary fix dues to a flet issue (https://github.com/flet-dev/flet/issues/2637)
