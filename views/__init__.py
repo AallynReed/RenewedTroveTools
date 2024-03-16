5d71542321e1454bfea823fa2011e9f2b288946d
Sly <sly@slynx.xyz>
2024-03-16 23:11:53 +0000

    Reordered views

--- views/__init__.py
+++ views/__init__.py
@@ -7,16 +7,16 @@ from views.no_tab import *
 def all_views(web_mode=False):
     views = list()
     views.append(HomeView)
+    if not web_mode:
+        views.append(ModsView)
+        views.append(ExtractorView)
+        views.append(LoginView)
     views.append(StarView)
     views.append(GemBuildsView)
     views.append(GemView)
     views.append(GemSetView)
     views.append(MasteryView)
     views.append(MagicFindView)
-    if not web_mode:
-        views.append(ModsView)
-        views.append(ExtractorView)
-        views.append(LoginView)
     if web_mode:
         views.append(View404)
     return views
