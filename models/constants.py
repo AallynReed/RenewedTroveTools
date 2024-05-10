fa41a66ebeda356ffad175599cb98bca0b217ca9
Sly <darkinleviathan@gmail.com>
2024-05-10 13:21:36 +0100

    Added an option in dev to load local files over network ones

--- models/constants.py
+++ models/constants.py
@@ -6,47 +6,63 @@ from base64 import b64decode
 files_cache = {}
 
 
-async def fetch_files():
+async def fetch_files(local_data=False, local_locales=False):
     async with ClientSession() as session:
         # Load data files
         files_cache.clear()
-        response = await session.get("https://kiwiapi.slynx.xyz/v1/stats/get_data")
-        if response.status != 200:
+        if not local_data:
+            response = await session.get("https://kiwiapi.slynx.xyz/v1/stats/get_data")
+            if response.status != 200:
+                data_path = Path("data")
+                files_data = [
+                    str(x.relative_to(data_path))
+                    for x in data_path.rglob("*")
+                    if x.is_file()
+                ]
+                files_cache.update(
+                    {
+                        path.replace("\\", "/"): load(
+                            open(data_path.joinpath(path), encoding="utf-8")
+                        )
+                        for path in files_data
+                    }
+                )
+            else:
+                files_data = await response.json()
+                for path, data in files_data.items():
+                    files_cache[path] = loads(b64decode(data).decode("utf-8"))
+        else:
             data_path = Path("data")
-            files_data = [
-                str(x.relative_to(data_path))
-                for x in data_path.rglob("*")
-                if x.is_file()
-            ]
-            files_cache.update(
-                {
-                    path.replace("\\", "/"): load(
-                        open(data_path.joinpath(path), encoding="utf-8")
-                    )
-                    for path in files_data
-                }
-            )
-        else:
-            files_data = await response.json()
-            for path, data in files_data.items():
-                files_cache[path] = loads(b64decode(data).decode("utf-8"))
+            for x in data_path.rglob("*.json"):
+                if x.is_file():
+                    file_name = str(x.relative_to(data_path).as_posix())
+                    files_cache[file_name] = loads(x.read_text(encoding="utf-8"))
         # Load localization files
-        response = await session.get("https://kiwiapi.slynx.xyz/v1/misc/locales")
-        if response.status != 200:
-            locales_path = Path("locales")
-            files_data = [
-                str(x.relative_to(locales_path))
-                for x in locales_path.rglob("*")
-                if x.is_file()
-            ]
-            files_cache.update(
-                {
-                    path.replace("\\", "/"): locales_path.joinpath(path).read_text(encoding="utf-8")
-                    for path in files_data
-                }
-            )
+        if not local_locales:
+            response = await session.get("https://kiwiapi.slynx.xyz/v1/misc/locales")
+            if response.status != 200:
+                locales_path = Path("locales")
+                files_data = [
+                    str(x.relative_to(locales_path))
+                    for x in locales_path.rglob("*")
+                    if x.is_file()
+                ]
+                files_cache.update(
+                    {
+                        path.replace("\\", "/"): locales_path.joinpath(path).read_text(
+                            encoding="utf-8"
+                        )
+                        for path in files_data
+                    }
+                )
+            else:
+                files_data = await response.json()
+                for path, data in files_data.items():
+                    files_cache[path] = b64decode(data).decode("utf-8")
         else:
-            files_data = await response.json()
-            for path, data in files_data.items():
-                files_cache[path] = b64decode(data).decode("utf-8")
+            locales_path = Path("locales")
+            for x in locales_path.rglob("*.loc"):
+                if x.is_file():
+                    file_name = str(x.relative_to(locales_path).as_posix())
+                    files_cache[file_name] = x.read_text(encoding="utf-8")
         return files_cache
