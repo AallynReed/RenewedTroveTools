5920451ddfff6deab9d11be93f21d7867cbbb7d5
Sly <darkinleviathan@gmail.com>
2024-05-24 17:26:49 +0100

    Fixed image fallbacks

--- models/interface/image.py
+++ models/interface/image.py
@@ -6,13 +6,13 @@ class RTTImage(Image):
         if is_fallback:
             super().__init__(**kwargs)
             return
-        if src is not None and isinstance(src, str) and src.startswith("assets/"):
+        if isinstance(src, str) and src.startswith("assets/"):
             src = "https://kiwiapi.slynx.xyz/v1/misc/" + src
         kwargs["src"] = src
         if kwargs.get("error_content") is None:
             error_content = RTTImage(
-                is_fallback=True,
                 src="https://kiwiapi.slynx.xyz/v1/misc/assets/images/construction.png",
+                is_fallback=True,
             )
             kwargs["error_content"] = error_content
         super().__init__(**kwargs)
