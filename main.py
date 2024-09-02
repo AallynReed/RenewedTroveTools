7c2d7c29ca05ad8fa5ec931254aecd888830d161
Aallyn <contact@aallyn.xyz>
2024-09-02 19:45:53 +0100

    Upped version

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ class App:
             response = requests.get(
                 "https://kiwiapi.aallyn.xyz/v1/user/discord/get?pass_key=" + token,
                 timeout=2,
-                headers=headers
+                headers=headers,
             )
             if response.status_code != 200:
                 fail = True
